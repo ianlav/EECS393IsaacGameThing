a65a6864094818487e7e730f806fb9078a2258c8
ian lavelle <ian lavelle>
2016-09-04 19:53:59 -0400

    Basic left/right movement. Jumping half implemented. Basic color sprites.

---
+++ EECS393IsaacGameThing/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float horizontalAccel;
+
+    private Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+    // Fixed Update is called every fixed amount of time
+    void FixedUpdate()
+    {
+        if(Input.GetButton("MoveLeft"))
+        {
+            rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, -horizontalSpeed, horizontalAccel), rigid.velocity.y);
+        } else if (Input.GetButton("MoveRight"))
+        {
+            rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, horizontalSpeed, horizontalAccel), rigid.velocity.y);
+        } else {
+            rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, 0, horizontalAccel), rigid.velocity.y);
+        }
+    }
+}
