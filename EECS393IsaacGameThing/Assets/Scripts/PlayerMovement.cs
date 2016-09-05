b6ece6bda59d155236a8c8b457be498cbc5787fa
ianlav <irl@case.edu>
2016-09-04 20:23:04 -0400

    Added jumping More comments to explain the basics:wq

--- EECS393IsaacGameThing/Assets/Scripts/PlayerMovement.cs
+++ EECS393IsaacGameThing/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+    //any public values can be modified easily in the inspector
     public float horizontalSpeed;
     public float horizontalAccel;
+    public float jumpSpeed;
 
     private Rigidbody2D rigid;
+    private bool isJumping = true;
 
 	// Use this for initialization
 	void Start () {
@@ -21,14 +24,30 @@ public class PlayerMovement : MonoBehaviour {
     // Fixed Update is called every fixed amount of time
     void FixedUpdate()
     {
+        //basic lateral movement. Linsearly interpolates the velocity between current and intended velocity.
         if(Input.GetButton("MoveLeft"))
         {
             rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, -horizontalSpeed, horizontalAccel), rigid.velocity.y);
-        } else if (Input.GetButton("MoveRight"))
-        {
+        } else if (Input.GetButton("MoveRight")) {
             rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, horizontalSpeed, horizontalAccel), rigid.velocity.y);
         } else {
             rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, 0, horizontalAccel), rigid.velocity.y);
+        }
+
+        //just adds a vertical velocity
+        if(Input.GetButton("Jump") && !isJumping)
+        {
+            isJumping = true;
+            rigid.velocity += new Vector2(0, jumpSpeed);
+        }
+    }
+
+    //is run whenever this object enters a collision with another 2D object. Self explanatory but yeah
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Floor")) //this checks the objects tag (directly below its name in the inspector panel). Just a quick way to check what something is
+        {
+            isJumping = false;
         }
     }
 }
