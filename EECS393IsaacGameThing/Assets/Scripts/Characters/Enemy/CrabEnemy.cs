f3fe749af8a312ba245e3383bbd6e9c3d61773bf
Rebecca Haluska <rmh111@case.edu>
2016-10-25 17:15:23 -0400

    Adds implementation for abstract Enemy & Enemy itself

---
+++ EECS393IsaacGameThing/Assets/Scripts/Characters/Enemy/CrabEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class CrabEnemy : Enemy{
+
+	//Not sure of the accepted way of doing this, but it'll work for Round 1.
+	new public static int cost = 1;
+	new public static double maxSpeed = 0;
+	new public static double baseDamage = 5;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		//Eventually we'll have *moving* monsters.
+	}
+
+
+}
+
---
+++ EECS393IsaacGameThing/Assets/Scripts/Characters/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Enemy : MonoBehaviour {
+
+	public int cost; //How much it "costs" to spawn the enemy. Higher for larger/dangerous enemies. Affects score.
+	public double maxSpeed; //Max distance moved per update.
+	public int baseDamage; //Damage dealt by bullet/on contact without any modifiers
+	public EnemyMaker maker;
+
+	void Start() {
+		maker = FindObjectOfType<EnemyMaker>();
+	}
+
+	void Update () {}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.CompareTag("LevelTrigger")){
+			maker.setCurrentCost (maker.getCurrentCost() - cost); //recycle monster's cost
+			Destroy(gameObject, 3);	
+		}
+	}
+
+}
