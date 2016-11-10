8a5ff300eab5a8ea700d997e2b4696f307967300
Rebecca Haluska <rmh111@case.edu>
2016-11-09 20:42:19 -0500

    Started implementation of in-game score tracking

---
+++ EECS393IsaacGameThing/Assets/Scripts/User Interface/ScoreController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	UIController controller;
+	Text scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+		scoreDisplay = GetComponent<Text>();
+		scoreDisplay.text = "Score: "; //+ controller.getCurrentScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreDisplay.text = "Score: "; //+ controller.getCurrentScore();
+	}
+}
--- EECS393IsaacGameThing/Assets/Scripts/User Interface/UIController.cs
+++ EECS393IsaacGameThing/Assets/Scripts/User Interface/UIController.cs
@@ -3,9 +3,19 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
+
+	int currentScore = 0;
 	
     public void ChangeScene (string sceneName)
     {
         SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
+
+	public void setCurrentScore(int newScore){
+		this.currentScore = newScore;
+	}
+
+	public int getCurrentScore(){
+		return currentScore;
+	}
 }
