32e5069ce448471dad08eb5245d11451079ea455
Aaron Cutright <ahc49@case.edu>
2016-11-11 01:33:20 -0500

    Made test more robust
    Added condition to verify double-spending fix didn't break anything

--- EECS393IsaacGameThing/Assets/Tests/Weapons/Editor/WeaponTests.cs
+++ EECS393IsaacGameThing/Assets/Tests/Weapons/Editor/WeaponTests.cs
@@ -33,12 +33,17 @@ public class WeaponTests {
         listOut = merge.mergeIfPossible(listIn);
         Assert.AreEqual(listOut[0].GetType(), typeof(LaserGun));
 
-        //test for repeated & unordered 5->1 merging (where 1 repeated value is missing)
+        //test for repeated & unordered 5->1 merging (where 1 repeated value is missing, verifies no double-spending)
         merge.clearMergeRules();
         Assert.IsTrue(merge.addMergeRule(new[] { typeof(StartGun), typeof(StartGun), typeof(SpreadGun), typeof(StartGun), typeof(SpreadGun) }, typeof(LaserGun)));
         listIn = new Weapon[] { gameObject.AddComponent<SpreadGun>(), gameObject.AddComponent<SpreadGun>(), gameObject.AddComponent<StartGun>(), gameObject.AddComponent<StartGun>(), };
         listOut = merge.mergeIfPossible(listIn);
         Assert.AreNotEqual(listOut[0].GetType(), typeof(LaserGun));
+
+        //test to make sure null's for double-spending don't break merge rules by accident (retest valid 5->1)
+        listIn = new Weapon[] { gameObject.AddComponent<SpreadGun>(), gameObject.AddComponent<SpreadGun>(), gameObject.AddComponent<StartGun>(), gameObject.AddComponent<StartGun>(), gameObject.AddComponent<StartGun>(), };
+        listOut = merge.mergeIfPossible(listIn);
+        Assert.AreEqual(listOut[0].GetType(), typeof(LaserGun));
 
         //test invalid merge options
         merge.clearMergeRules();
