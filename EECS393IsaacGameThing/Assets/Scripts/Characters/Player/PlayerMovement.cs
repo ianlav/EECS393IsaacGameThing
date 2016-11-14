c888889fb43bb13f22ba75b0676c6768acd9f955
ian lavelle <ian lavelle>
2016-11-14 15:21:40 -0500

    Player now stops jumping early if the button is let go quickly There are now three platform types: normal, icy, and bouncy

--- EECS393IsaacGameThing/Assets/Scripts/Characters/Player/PlayerMovement.cs
+++ EECS393IsaacGameThing/Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -48,8 +48,6 @@ public class PlayerMovement : CharacterModel {
             rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, -horizontalSpeed, horizontalAccel), rigid.velocity.y);
         } else if (Input.GetButton("MoveRight")) {
             rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, horizontalSpeed, horizontalAccel), rigid.velocity.y);
-        } else {
-            rigid.velocity = new Vector2(Mathf.Lerp(rigid.velocity.x, 0, horizontalAccel), rigid.velocity.y);
         }
 
         //just adds a vertical velocity
@@ -58,6 +56,11 @@ public class PlayerMovement : CharacterModel {
         {
             isJumping = true;
             rigid.velocity += new Vector2(0, jumpSpeed);
+        }
+
+        if(Input.GetButtonUp("Jump") && rigid.velocity.y > 0)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, 0);
         }
     }
 
