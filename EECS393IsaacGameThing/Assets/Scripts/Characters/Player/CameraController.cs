6349248c0494ad3f9fc55cfe88e89498e6a20e33
ian lavelle <ian lavelle>
2016-11-14 22:03:15 -0500

    Camera now lerps with the player's position, giving it a more dynamic feel

---
+++ EECS393IsaacGameThing/Assets/Scripts/Characters/Player/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    PlayerMovement player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = Vector2.Lerp((Vector2)player.transform.position, (Vector2)transform.position, 0.95f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -2);
+	}
+}
