dbd041c3c7b387874406876e7109f86775b8423d
ian lavelle <ian lavelle>
2016-11-30 15:06:20 -0500

    High score screen prints scores correctly Doesnt persist between sessions

---
+++ EECS393IsaacGameThing/Assets/Scripts/User Interface/HighScoreScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections;
+
+public class HighScoreScreen : MonoBehaviour {
+
+    public Text highScores;
+
+	// Use this for initialization
+	void Start () {
+        ReadOnlyCollection<Scoring.ScoreRecord> scores = Scoring.GetScores();
+        print(scores.Count);
+        for (int i = 0; i < scores.Count; i++)
+        {
+            print(scores[i].name);
+            highScores.text += (scores[i].name + ":  " + scores[i].score + "\n");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
